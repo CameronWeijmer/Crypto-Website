49fb78e1f9849d677264b70a5a2036612f74229a
Cameron Weijmer <weijmer.c@gmail.com>
2022-06-02 13:43:15 +0200

    Added Btc price

---
+++ javascript/app.en.js
@@ -0,0 +1,13 @@
+let btc = document.getElementById("bitcoin_price");
+
+fetch(
+  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+)
+.then(res => {
+    return res.json();
+}).then(data =>{
+    // console.log(data.bitcoin.usd);
+    btc.innerText = "$" + data.bitcoin.usd;
+}).catch(err=>{
+    alert("At the moment")
+})

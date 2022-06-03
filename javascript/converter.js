e6709a9791205f018fcb7f361e14e0586a18195f
Cameron Weijmer <weijmer.c@gmail.com>
2022-06-03 08:29:23 +0200

    coin dropdown list

--- javascript/converter.js
+++ javascript/converter.js
@@ -1,12 +1,18 @@
 let coins = [];
+const select = document.getElementById("selectCurrencies");
 
-await fetch(
+fetch(
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
 )
   .then((res) => res.json())
   .then((data) => {
+    select.innerHTML = "";
     data.forEach((coin) => {
-      coins.push(coin.id);
+      const option = document.createElement("option");
+      option.value = coin.id;
+      option.innerText = coin.name;
+      select.appendChild(option);
+      coins.push(coin);
     });
     return coins;
   });

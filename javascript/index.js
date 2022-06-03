bb90439ac80b214050edf89b9f532d4117009f09
Cameron <weijmer.c@gmail.com>
2022-06-03 22:47:46 +0200

    conten visibility

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,17 @@
-let menuList = document.getElementById('menuList');
+let menuList = document.getElementById("menuList");
+const content_block = document.getElementById("content_block");
 
 menuList.style.maxHeight = "0px";
 
 // Function to toggle the menulist
-function togglemenu(){
-    if(menuList.style.maxHeight == "0px") {
-        menuList.style.maxHeight = "500px";
-    }
-    else {
-        menuList.style.maxHeight = "0px";
-    }
+function togglemenu() {
+  if (menuList.style.maxHeight == "0px") {
+    menuList.style.maxHeight = "500px";
+    menuList.style.visibility = "visible";
+    content_block.style.visibility = "hidden";
+  } else {
+    content_block.style.visibility = "visible";
+    menuList.style.visibility = "hidden";
+    menuList.style.maxHeight = "0px";
+  }
 }

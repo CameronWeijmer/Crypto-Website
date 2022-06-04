7cb4de684f3476e37f2b4f880ff9f9ddb3307a0c
Cameron <weijmer.c@gmail.com>
2022-06-05 01:39:07 +0200

    implemented crypto

---
+++ javascript/crypto.js
@@ -0,0 +1,40 @@
+let baseUrl = "https://api.coinranking.com/v2/coins";
+let proxyUrl = "https://cors-anywhere.herokuapp.com/";
+let apiKey = "coinranking4631015331570a2b80854ae86b90f47ec74781ca0d5b0927";
+
+let apiUrl = `${proxyUrl}${baseUrl}`;
+console.log(apiUrl);
+
+fetch(`${proxyUrl}${baseUrl}`, {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    "X-My-Custom-Header": `${apiKey}`,
+    "Access-Control-Allow-Origin": "*",
+  },
+})
+  .then((response) => {
+    if (response.ok) {
+      response.json().then((json) => {
+        console.log(json.data);
+        let coinsData = json.data.coins;
+
+        if (coinsData.length > 0) {
+          var coinlist = "";
+        }
+
+        coinsData.forEach((coin) => {
+          coinlist += "<tr>";
+          coinlist += `<td id="coin_name"> ${coin.name}</td>`;
+          coinlist += `<td> $${coin.price}</td>`;
+          coinlist += `<td> ${coin["24hVolume"]} </td>`;
+          coinlist += `<td> ${coin.symbol} </td>`;
+          coinlist += `<td> ${coin.rank} </td>`;
+        });
+        document.getElementById("content").innerHTML = coinlist;
+      });
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
